0bdadc6719339caf885d350497662c381016aa62
nsetzler <nsetzler@yahoo.com>
2026-08-12 16:11:59 -0400

    change evt-status.service.ts

--- src/app/services/evt-status.service.ts
+++ src/app/services/evt-status.service.ts
@@ -189,35 +189,16 @@ export class EVTStatusService {
             }
         });
 
- this.editionContext.editionChange$.subscribe(() => {
-    if (this.isOnHomeRoute()) {
-        return;
-    }
-    const { viewModeId } = this.parseEditionRoute(this.router.url);
-    const vmFromUrl = viewModeId
-        ? this.availableViewModes.find((v) => v.id === viewModeId)
-        : undefined;
-    this.updateViewMode$.next(vmFromUrl ?? this.defaultViewMode);
-});
-
-to:
-
-this.editionContext.editionChange$.subscribe(() => {
-    if (this.isOnHomeRoute()) {
-        return;
-    }
-
-    // clear previous edition state
-    this.updateDocument$.next('');
-    this.updatePageId$.next('');
-
-    const { viewModeId } = this.parseEditionRoute(this.router.url);
-    const vmFromUrl = viewModeId
-        ? this.availableViewModes.find((v) => v.id === viewModeId)
-        : undefined;
-
-    this.updateViewMode$.next(vmFromUrl ?? this.defaultViewMode);
-});
+        this.editionContext.editionChange$.subscribe(() => {
+            if (this.isOnHomeRoute()) {
+                return;
+            }
+            const { viewModeId } = this.parseEditionRoute(this.router.url);
+            const vmFromUrl = viewModeId
+                ? this.availableViewModes.find((v) => v.id === viewModeId)
+                : undefined;
+            this.updateViewMode$.next(vmFromUrl ?? this.defaultViewMode);
+        });
 
         this.currentNamedEntityId$.pipe(
             filter((id) => !!id),
