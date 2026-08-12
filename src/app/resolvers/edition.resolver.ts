2c037c510c062a44ee7e16f6cb723d4fa2914416
nsetzler <nsetzler@yahoo.com>
2026-08-12 19:21:00 -0400

    revert

--- src/app/resolvers/edition.resolver.ts
+++ src/app/resolvers/edition.resolver.ts
@@ -20,24 +20,8 @@ export class EditionResolver implements Resolve<SiteEditionEntry> {
     const entry = this.editionContext.getEditionEntry(slug);
     const fileConfigUrl = this.editionContext.getFileConfigUrl(entry.configBase);
 
-    console.log('[EVT] RESOLVER START');
-    console.log('[EVT] slug:', slug);
-    console.log('[EVT] configBase:', entry.configBase);
-    console.log('[EVT] fileConfigUrl:', fileConfigUrl);
-
     return from(this.appConfig.loadEditionBundle(fileConfigUrl)).pipe(
-      tap(() => {
-        console.log('[EVT] BUNDLE LOADED');
-        console.log('[EVT] AppConfig editionUrls:',
-          AppConfig.evtSettings?.files?.editionUrls);
-        console.log('[EVT] AppConfig editionTitle:',
-          AppConfig.evtSettings?.edition?.editionTitle);
-
-        this.editionContext.setActiveEdition(entry);
-
-        console.log('[EVT] ACTIVE EDITION:',
-          this.editionContext.activeSlug);
-      }),
+      tap(() => this.editionContext.setActiveEdition(entry)),
       map(() => entry),
     );
   }
