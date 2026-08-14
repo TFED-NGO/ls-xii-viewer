3c17af13ce6621fed017eea8d8349451b4dcf8c9
nsetzler <nsetzler@yahoo.com>
2026-08-14 14:28:36 -0400

    test

--- src/app/guards/edition-slug.guard.ts
+++ src/app/guards/edition-slug.guard.ts
@@ -1,22 +1,13 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
-import { EditionContextService } from '../services/edition-context.service';
+canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
+  const slug = route.paramMap.get('editionSlug');
 
-@Injectable({
-  providedIn: 'root',
-})
-export class EditionSlugGuard implements CanActivate {
-  constructor(
-    private editionContext: EditionContextService,
-    private router: Router,
-  ) {}
+  console.log('EDITION GUARD:', slug);
+  console.log('SITE CONFIG:', this.editionContext.siteConfig);
+  console.log('VALID SLUG:', slug ? this.editionContext.isValidSlug(slug) : false);
 
-  canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
-    const slug = route.paramMap.get('editionSlug');
-    if (slug && this.editionContext.isValidSlug(slug)) {
-      return true;
-    }
-
-    return this.router.createUrlTree(['/']);
+  if (slug && this.editionContext.isValidSlug(slug)) {
+    return true;
   }
+
+  return this.router.createUrlTree(['/']);
 }
