4d763bfcff3c86f433f066a9686349fb2007d038
nsetzler <nsetzler@yahoo.com>
2026-08-14 14:34:23 -0400

    fix error

--- src/app/guards/edition-slug.guard.ts
+++ src/app/guards/edition-slug.guard.ts
@@ -1,13 +1,30 @@
-canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
-  const slug = route.paramMap.get('editionSlug');
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
+import { EditionContextService } from '../services/edition-context.service';
 
-  console.log('EDITION GUARD:', slug);
-  console.log('SITE CONFIG:', this.editionContext.siteConfig);
-  console.log('VALID SLUG:', slug ? this.editionContext.isValidSlug(slug) : false);
+@Injectable({
+  providedIn: 'root',
+})
+export class EditionSlugGuard implements CanActivate {
+  constructor(
+    private editionContext: EditionContextService,
+    private router: Router,
+  ) {}
 
-  if (slug && this.editionContext.isValidSlug(slug)) {
-    return true;
+  canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
+    const slug = route.paramMap.get('editionSlug');
+
+    console.log('EDITION GUARD:', slug);
+    console.log('SITE CONFIG:', this.editionContext.siteConfig);
+    console.log(
+      'VALID SLUG:',
+      slug ? this.editionContext.isValidSlug(slug) : false
+    );
+
+    if (slug && this.editionContext.isValidSlug(slug)) {
+      return true;
+    }
+
+    return this.router.createUrlTree(['/']);
   }
-
-  return this.router.createUrlTree(['/']);
 }
